348ffca1f13b53738dbcbaecf7dabccb3ae0ff71
Pooja <poojasawant1030@gmail.com>
2024-08-05 01:01:32 +0530

    first commit

---
+++ frontend/src/Components/Description/Description.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Description.css'
+
+
+const Description = () => {
+  return (
+    <div className='description'>
+      <div className='description-navigator'>
+        <div className='description-nav-box'>Description</div>
+        <div className='description-nav-box fade'>Reviews (122)</div>
+      </div>
+      <div className='description-description'>
+        <p>An e-commerce</p>
+        <p>E-commerce</p>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Description
